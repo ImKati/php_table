b1ee3ee777f89485368e858e903ff7b596e6e917
ImKati <kati.mihalevich@gmail.com>
2023-03-21 17:42:38 +0300

    post

--- index.php
+++ index.php
@@ -11,12 +11,21 @@
 
 </head>
 
+<body>
+<form action="index.php" method="post">
+    <input type="text" name="name" placeholder="Имя"><br><br>
+    <input type="text" name="age" placeholder="Возраст"><br><br>
+    <input type="text" name="salary" placeholder="Зарплата"><br><br>
+    <button type="submit">Create</button>
+</form>
+</body>
+
 <?php
 $host = 'localhost';
 $user = 'root';
 $password = '12345';
 $db_name = 'users';
-$conn = new mysqli($host, $user, $password, $db_name) or die(mysqli_error($connections));
+$conn = new mysqli($host, $user, $password, $db_name) or die(mysqli_error($conn));
 
 if ($conn->connect_error) {
     die("ERROR: Unable to connect: " . $conn->connect_error);
@@ -30,7 +39,7 @@ echo "Number of rows: $result->num_rows";
 
 echo "<table class='tab'>";
 
-while($user = mysqli_fetch_assoc($result)){
+while ($user = mysqli_fetch_assoc($result)) {
     echo "<tr>
     <td>" . $user['id'] . "</td>
     <td>" . $user['name'] . "</td>
@@ -40,9 +49,10 @@ while($user = mysqli_fetch_assoc($result)){
 </tr>";
 }
 
-if(isset($_GET['del'])) {
+echo "</table>";
+
+if (isset($_GET['del'])) {
     $del = (int)$_GET['del'];
-    $query = "DELETE FROM users WHERE id={'$del'}";
     $result = $conn->query("DELETE FROM users where id={$del}");
 
     if (!$result) {
@@ -52,4 +62,19 @@ if(isset($_GET['del'])) {
     header("Location: index.php");
 }
 
-echo"</table>";
+if (isset($_POST['name']) && isset($_POST['age']) && isset($_POST['salary'])) {
+
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $salary = $_POST['salary'];
+    //echo "INSERT INTO users (name, age , salary) values ($name, $age, $salary)";
+    $result = $conn->query("INSERT INTO users (name, age , salary) values ('$name', '$age', '$salary')");
+
+    header("Location: index.php");
+}
+
+
+
+
+
+
