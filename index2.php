474332427c4677f9e736b90d5e3a10905b4bf455
ImKati <kati.mihalevich@gmail.com>
2023-03-27 15:00:52 +0300

    editing

--- index2.php
+++ index2.php
@@ -1,24 +1,21 @@
 <!doctype html>
 <html lang="en">
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Users</title>
-
+    <title>Document</title>
 </head>
-
 <body>
 <form action="index.php" method="post">
     <input type="text" name="name" placeholder="Имя"><br><br>
     <input type="text" name="age" placeholder="Возраст"><br><br>
     <input type="text" name="salary" placeholder="Зарплата"><br><br>
-    <button type="submit">Выполнить</button>
+    <button type="submit">Изменить</button>
 </form>
 </body>
+</html>
 
 <?php
 $host = 'localhost';
@@ -40,29 +37,7 @@ echo "Number of rows: $result->num_rows";
 echo "<table class='tab'>";
 
 while ($user = mysqli_fetch_assoc($result)) {
-    echo "<tr>
-    <td>" . $user['id'] . "</td>
-    <td>" . $user['name'] . "</td>
-    <td>" . $user['age'] . "</td>
-    <td>" . $user['salary'] . "</td>
-    <td><a href='index.php?del={$user['id']}'>Удалить</a></td>
-    <td><a href= 'index2.php' target='_blank'>Редактировать</href></td>
-    
-</tr>";
-}
 
-
-echo "</table>";
-
-if (isset($_GET['del'])) {
-    $del = (int)$_GET['del'];
-    $result = $conn->query("DELETE FROM users where id={$del}");
-
-    if (!$result) {
-        die("Database query failed");
-    }
-
-    header("Location: index.php");
 }
 
 if (isset($_POST['name']) && isset($_POST['age']) && isset($_POST['salary'])) {
@@ -74,10 +49,4 @@ if (isset($_POST['name']) && isset($_POST['age']) && isset($_POST['salary'])) {
 
     header("Location: index.php");
 }
-
-
-
-
-
-
 
